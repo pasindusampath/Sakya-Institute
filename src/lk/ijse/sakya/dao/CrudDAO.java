45a53c2e644864a068838884822260efd6b83c4a
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-01-18 22:44:50 +0530

    implements USER DAO

--- src/lk/ijse/sakya/dao/CrudDAO.java
+++ src/lk/ijse/sakya/dao/CrudDAO.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public interface CrudDAO<T,ID> extends SuperDAO{
     boolean add(T obj) throws SQLException, ClassNotFoundException;
-    boolean update(T obj);
+    boolean update(T obj) throws SQLException, ClassNotFoundException;
     T search(ID id) throws SQLException, ClassNotFoundException;
-    boolean delete(ID id);
+    boolean delete(ID id) throws SQLException, ClassNotFoundException;
 }
--- src/lk/ijse/sakya/dao/custom/impl/UserDAOImpl.java
+++ src/lk/ijse/sakya/dao/custom/impl/UserDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.sakya.dao.custom.impl;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import lk.ijse.sakya.dao.custom.UserDAO;
 import lk.ijse.sakya.entity.custom.User;
@@ -17,8 +18,10 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public boolean update(User obj) {
-        return false;
+    public boolean update(User user) throws SQLException, ClassNotFoundException {
+        return CrudUtil.execute("update user set name = ? ,type = ? ,gmail = ? ,contact = ? ,dob = ? ," +
+                        "address = ? where id = ?", user.getName(), user.getType(), user.getGmail(),
+                user.getContact(), user.getDob(), user.getAddress(), user.getId());
     }
 
     @Override
@@ -33,8 +36,8 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String s) throws SQLException, ClassNotFoundException {
+        return CrudUtil.execute("delete from user where id = ?", s);
     }
 
     @Override
@@ -50,31 +53,76 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public String getNewUserId() throws SQLException, ClassNotFoundException {
-        return null;
+        String lastUserId = getLastUserId();
+        if (lastUserId == null) {
+            return "U-001";
+        } else {
+            String[] split = lastUserId.split("[U][-]");
+            int lastDigits = Integer.parseInt(split[1]);
+            lastDigits++;
+            String newUserId = String.format("U-%03d", lastDigits);
+            return newUserId;
+        }
     }
 
     @Override
     public String getLastUserId() throws SQLException, ClassNotFoundException {
+        ResultSet rs = CrudUtil.execute("SELECT id from user order by id DESC limit 1");
+        if (rs.next()) {
+            return rs.getString(1);
+        }
         return null;
     }
 
     @Override
     public ObservableList<User> getAllUsers() throws SQLException, ClassNotFoundException {
-        return null;
+        ObservableList<User> ob = FXCollections.observableArrayList();
+        ResultSet rs = CrudUtil.execute("SELECT * FROM USER");
+        while (rs.next()) {
+            ob.add(new User(rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6),
+                    rs.getString(7),
+                    rs.getString(8)));
+        }
+        return ob;
     }
 
     @Override
     public ObservableList<User> getTeachers() throws SQLException, ClassNotFoundException {
-        return null;
+        ObservableList<User> ob = FXCollections.observableArrayList();
+        ResultSet rs = CrudUtil.execute("SELECT * from user where type = 'teacher'");
+        while (rs.next()) {
+            ob.add(new User(rs.getString(1), rs.getString(2), rs.getString(3)
+                    , rs.getString(4), rs.getString(5), rs.getString(6)
+                    , rs.getString(7), rs.getString(8)));
+        }
+        return ob;
     }
 
     @Override
     public boolean resetPassword(User user) throws SQLException, ClassNotFoundException {
-        return false;
+        return CrudUtil.execute("update user set password = ? where id = ?",user.getPassword(),user.getId());
     }
 
     @Override
     public ObservableList<User> searchUser(String searchBy, String text) throws SQLException, ClassNotFoundException {
-        return null;
+        String sql= "SELECT * from user where "+searchBy+" like '%"+text+"%'";
+        ResultSet rs = CrudUtil.execute(sql);
+        ObservableList<User> list = FXCollections.observableArrayList();
+        while(rs.next()){
+            list.add(new User(rs.getString(1),
+                    rs.getString(2),
+                    rs.getString(3),
+                    rs.getString(4),
+                    rs.getString(5),
+                    rs.getString(6),
+                    rs.getString(7),
+                    rs.getString(8)));
+        }
+        return list;
     }
 }
